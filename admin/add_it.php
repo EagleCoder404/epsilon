fce7dd2bf85929ec1eb790a4e9e2a55420259ee4
EagleCoder404 <harsha.jediknight@gmail.com>
2020-04-03 18:55:58 +0530

    sss

--- admin/add_it.php
+++ admin/add_it.php
@@ -11,10 +11,12 @@ $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 
 
 $res = $con->query('select max(number) as n from quiz;')->fetch_assoc();
+
 if($res['n']==NULL)
     $num=1;
 else
     $num=$res['n']+1;
+
 $q = addslashes($q);
 $a = addslashes($a);
 $sql = "insert into quiz values($num,'$q','$a')";
@@ -22,7 +24,7 @@ if($con->query($sql)===True)
     echo "question added";
 else
     echo $con->error."<br>";
-
+echo "<br>$imageFileType<br>";
     if (move_uploaded_file($_FILES["image"]["tmp_name"], "../data/"."$num.".$imageFileType)) {
         echo "The file ". basename( $_FILES["image"]["name"]). " has been uploaded.";
     } else {
