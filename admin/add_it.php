56bc66309658ceecd43736b764b711b178066624
EagleCoder404 <harsha.jediknight@gmail.com>
2020-04-03 18:07:06 +0530

    s

--- admin/add_it.php
+++ admin/add_it.php
@@ -18,6 +18,7 @@ if($con->query($sql)===True)
     echo "question added";
 else
     echo $con->error."<br>";
+    
     if (move_uploaded_file($_FILES["image"]["tmp_name"], "../data/"."$num.".$imageFileType)) {
         echo "The file ". basename( $_FILES["image"]["name"]). " has been uploaded.";
     } else {
